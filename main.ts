78d184b7ca74a219040f32389a8786bc0e5f4a0f
LDPCICNM <78750096+LDPCICNM@users.noreply.github.com>
2021-03-08 18:20:30 +0100

    esto es para el robot mega frog

--- main.ts
+++ main.ts
@@ -1,10 +1,34 @@
-k_Bit.OFFLed()
-k_Bit.LED_brightness(0)
 irRemote.connectInfrared(DigitalPin.P16)
 let Landerito_guay_que_es_un_pro = 0
 basic.forever(function () {
     Landerito_guay_que_es_un_pro = irRemote.returnIrButton()
 })
 basic.forever(function () {
-    basic.showNumber(Landerito_guay_que_es_un_pro)
+    if (Landerito_guay_que_es_un_pro == 70) {
+        k_Bit.run(DIR.RunForward, 70)
+    }
+    if (Landerito_guay_que_es_un_pro == 21) {
+        k_Bit.run(DIR.RunBack, 70)
+    }
+    if (Landerito_guay_que_es_un_pro == 68) {
+        k_Bit.run(DIR.TurnLeft, 70)
+        basic.pause(2000)
+        k_Bit.carStop()
+    }
+    if (Landerito_guay_que_es_un_pro == 67) {
+        k_Bit.run(DIR.TurnRight, 70)
+        basic.pause(2000)
+        k_Bit.carStop()
+    }
+    if (Landerito_guay_que_es_un_pro == 64) {
+        k_Bit.carStop()
+    }
+    if (Landerito_guay_que_es_un_pro == 22) {
+        k_Bit.LED_brightness(113)
+        k_Bit.Led(COLOR.green)
+    }
+    if (Landerito_guay_que_es_un_pro == 25) {
+        k_Bit.LED_brightness(113)
+        k_Bit.Led(COLOR.blue)
+    }
 })
