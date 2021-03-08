62cf64f0d5116c0b08db5002f46ecdb882b2c5a2
LDPCICNM <78750096+LDPCICNM@users.noreply.github.com>
2021-03-08 17:56:14 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+k_Bit.OFFLed()
+k_Bit.LED_brightness(0)
+irRemote.connectInfrared(DigitalPin.P16)
+let Landerito_guay_que_es_un_pro = 0
 basic.forever(function () {
-	
+    Landerito_guay_que_es_un_pro = irRemote.returnIrButton()
+})
+basic.forever(function () {
+    basic.showNumber(Landerito_guay_que_es_un_pro)
 })
